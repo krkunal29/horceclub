112b8a5bff6014e2cf4385523c88e3bfe59922d7
krkunal29 <krkunal29@gmail.com>
2019-12-09 16:19:13 +0530

    all code

---
+++ horseclub/about.php
@@ -0,0 +1,304 @@
+	<!DOCTYPE html>
+	<html lang="zxx" class="no-js">
+	<head>
+		<!-- Mobile Specific Meta -->
+		<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+		<!-- Favicon-->
+		<link rel="shortcut icon" href="img/fav.png">
+		<!-- Author Meta -->
+		<meta name="author" content="codepixer">
+		<!-- Meta Description -->
+		<meta name="description" content="">
+		<!-- Meta Keyword -->
+		<meta name="keywords" content="">
+		<!-- meta character set -->
+		<meta charset="UTF-8">
+		<!-- Site Title -->
+		<title>Horse Club</title>
+
+		<!-- <link href="https://fonts.googleapis.com/css?family=Poppins:100,200,400,300,500,600,700" rel="stylesheet">  -->
+			<!--
+			CSS
+			============================================= -->
+			<link rel="stylesheet" href="css/linearicons.css">
+			<link rel="stylesheet" href="css/font-awesome.min.css">
+			<link rel="stylesheet" href="css/bootstrap.css">
+			<link rel="stylesheet" href="css/magnific-popup.css">
+			<link rel="stylesheet" href="css/nice-select.css">
+			<link rel="stylesheet" href="css/animate.min.css">
+			<!-- <link rel="stylesheet" href="https://code.jquery.com/ui/1.12.1/themes/base/jquery-ui.css">	 -->
+			<link rel="stylesheet" href="css/owl.carousel.css">
+			<link rel="stylesheet" href="css/main.css">
+		</head>
+		<body>
+        <?php include "header.php" ?>
+
+			<!-- start banner Area -->
+			<section class="banner-area relative" id="home">
+				<div class="overlay overlay-bg"></div>
+				<div class="container">
+					<div class="row d-flex align-items-center justify-content-center">
+						<div class="about-content col-lg-12">
+							<h1 class="text-white">
+								About Us
+							</h1>
+							<p class="text-white link-nav"><a href="index.html">Home </a>  <span class="lnr lnr-arrow-right"></span>  <a href="about.html"> About Us</a></p>
+						</div>
+					</div>
+				</div>
+			</section>
+			<!-- End banner Area -->
+
+			<!-- Start home-about Area -->
+			<section class="home-about-area section-gap">
+				<div class="container">
+					<div class="row">
+						<div class="col-lg-6 home-about-left">
+							<img class="mx-auto d-block img-fluid" src="img/about-img.png" alt="">
+						</div>
+						<div class="col-lg-6 home-about-right">
+							<h6 class="text-uppercase">Brand new app to blow your mind</h6>
+							<h1>We’ve made a life <br>
+							that will change you</h1>
+							<p>
+								<span>We are here to listen from you deliver exellence</span>
+							</p>
+							<p>
+								Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Sed ut perspiciatis unde omnis.
+							</p>
+							<a class="primary-btn" href="#">Get Started Now</a>
+						</div>
+					</div>
+				</div>
+			</section>
+			<!-- End home-about Area -->
+
+			<!-- Start feature Area -->
+			<section class="feature-area relative pt-100 pb-20">
+				<div class="overlay overlay-bg"></div>
+				<div class="container">
+					<div class="row align-items-center justify-content-center">
+						<div class="col-lg-4 col-md-6">
+							<div class="single-feature">
+								<a href="#"><h4 class="text-white">Expert Technicians</h4></a>
+								<p>
+									Usage of the Internet is becoming more common due to rapid advancement of technology and power.
+								</p>
+							</div>
+						</div>
+						<div class="col-lg-4 col-md-6">
+							<div class="single-feature">
+								<a href="#"><h4 class="text-white">Professional Service</h4></a>
+								<p>
+									Usage of the Internet is becoming more common due to rapid advancement of technology and power.
+								</p>
+							</div>
+						</div>
+						<div class="col-lg-4 col-md-6">
+							<div class="single-feature">
+								<a href="#"><h4 class="text-white">Great Support</h4></a>
+								<p>
+									Usage of the Internet is becoming more common due to rapid advancement of technology and power.
+								</p>
+							</div>
+						</div>
+						<div class="col-lg-4 col-md-6">
+							<div class="single-feature">
+								<a href="#"><h4 class="text-white">Technical Skills</h4></a>
+								<p>
+									Usage of the Internet is becoming more common due to rapid advancement of technology and power.
+								</p>
+							</div>
+						</div>
+						<div class="col-lg-4 col-md-6">
+							<div class="single-feature">
+								<a href="#"><h4 class="text-white">Highly Recomended</h4></a>
+								<p>
+									Usage of the Internet is becoming more common due to rapid advancement of technology and power.
+								</p>
+							</div>
+						</div>
+						<div class="col-lg-4 col-md-6">
+							<div class="single-feature">
+								<a href="#"><h4 class="text-white">Positive Reviews</h4></a>
+								<p>
+									Usage of the Internet is becoming more common due to rapid advancement of technology and power.
+								</p>
+							</div>
+						</div>
+					</div>
+				</div>
+			</section>
+			<!-- End feature Area -->
+
+			<!-- Start about-video Area -->
+			<section class="about-video-area section-gap">
+				<div class="container">
+					<div class="row">
+						<div class="col-lg-6 about-video-left">
+							<h6 class="text-uppercase">Brand new app to blow your mind</h6>
+							<h1>
+								We’ve made a life <br>
+								that will change you
+							</h1>
+							<p>
+								<span>We are here to listen from you deliver exellence</span>
+							</p>
+							<p>
+								Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed doeiusmo d tempor incididunt ut labore et dolore magna aliqua.
+							</p>
+							<a class="primary-btn" href="#">Get Started Now</a>
+						</div>
+						<div class="col-lg-6 about-video-right justify-content-center align-items-center d-flex">
+							<a class="play-btn" href="https://www.youtube.com/watch?v=ARA0AxrnHdM"><img class="img-fluid mx-auto" src="img/play.png" alt=""></a>
+						</div>
+					</div>
+				</div>
+			</section>
+			<!-- End about-video Area -->
+
+			<!-- Start booking Area -->
+			<section class="booking-area section-gap relative" id="consultancy">
+				<div class="overlay overlay-bg"></div>
+				<div class="container">
+					<div class="row justify-content-between align-items-center">
+						<div class="col-lg-6 col-md-6 booking-left">
+							<div class="active-review-carusel">
+								<div class="single-carusel">
+									<img src="img/r1.png" alt="">
+									<div class="title justify-content-start d-flex">
+										<h4>Fannie Rowe</h4>
+										<div class="star">
+											<span class="fa fa-star checked"></span>
+											<span class="fa fa-star checked"></span>
+											<span class="fa fa-star checked"></span>
+											<span class="fa fa-star"></span>
+											<span class="fa fa-star"></span>
+										</div>
+									</div>
+									<p>
+										Accessories Here you can find the best computer accessory for your laptop, monitor, printer, scanner, speaker. Here you can find the best computer accessory for your laptop, monitor, printer, scanner, speaker.
+									</p>
+									<img src="img/r2.png" alt="">
+									<div class="title justify-content-start d-flex">
+										<h4>Hulda Sutton</h4>
+										<div class="star">
+											<span class="fa fa-star checked"></span>
+											<span class="fa fa-star checked"></span>
+											<span class="fa fa-star checked"></span>
+											<span class="fa fa-star"></span>
+											<span class="fa fa-star"></span>
+										</div>
+									</div>
+									<p>
+										Accessories Here you can find the best computer accessory for your laptop, monitor, printer, scanner, speaker. Here you can find the best computer accessory for your laptop, monitor, printer, scanner, speaker.
+									</p>
+								</div>
+								<div class="single-carusel">
+									<img src="img/r1.png" alt="">
+									<div class="title justify-content-start d-flex">
+										<h4>Fannie Rowe</h4>
+										<div class="star">
+											<span class="fa fa-star checked"></span>
+											<span class="fa fa-star checked"></span>
+											<span class="fa fa-star checked"></span>
+											<span class="fa fa-star"></span>
+											<span class="fa fa-star"></span>
+										</div>
+									</div>
+									<p>
+										Accessories Here you can find the best computer accessory for your laptop, monitor, printer, scanner, speaker. Here you can find the best computer accessory for your laptop, monitor, printer, scanner, speaker.
+									</p>
+									<img src="img/r2.png" alt="">
+									<div class="title justify-content-start d-flex">
+										<h4>Hulda Sutton</h4>
+										<div class="star">
+											<span class="fa fa-star checked"></span>
+											<span class="fa fa-star checked"></span>
+											<span class="fa fa-star checked"></span>
+											<span class="fa fa-star"></span>
+											<span class="fa fa-star"></span>
+										</div>
+									</div>
+									<p>
+										Accessories Here you can find the best computer accessory for your laptop, monitor, printer, scanner, speaker. Here you can find the best computer accessory for your laptop, monitor, printer, scanner, speaker.
+									</p>
+								</div>
+								<div class="single-carusel">
+									<img src="img/r1.png" alt="">
+									<div class="title justify-content-start d-flex">
+										<h4>Fannie Rowe</h4>
+										<div class="star">
+											<span class="fa fa-star checked"></span>
+											<span class="fa fa-star checked"></span>
+											<span class="fa fa-star checked"></span>
+											<span class="fa fa-star"></span>
+											<span class="fa fa-star"></span>
+										</div>
+									</div>
+									<p>
+										Accessories Here you can find the best computer accessory for your laptop, monitor, printer, scanner, speaker. Here you can find the best computer accessory for your laptop, monitor, printer, scanner, speaker.
+									</p>
+									<img src="img/r2.png" alt="">
+									<div class="title justify-content-start d-flex">
+										<h4>Hulda Sutton</h4>
+										<div class="star">
+											<span class="fa fa-star checked"></span>
+											<span class="fa fa-star checked"></span>
+											<span class="fa fa-star checked"></span>
+											<span class="fa fa-star"></span>
+											<span class="fa fa-star"></span>
+										</div>
+									</div>
+									<p>
+										Accessories Here you can find the best computer accessory for your laptop, monitor, printer, scanner, speaker. Here you can find the best computer accessory for your laptop, monitor, printer, scanner, speaker.
+									</p>
+								</div>
+							</div>
+						</div>
+						<div class="col-lg-4 col-md-6 booking-right">
+								<h4 class="mb-20">Appointment Form</h4>
+								<form action="#">
+									<input class="form-control" type="text" name="name" placeholder="Your name" required>
+									<input class="form-control" type="email" name="email" placeholder="Email Address" required>
+									<input class="form-control" type="text" name="phone" placeholder="Phone Number" required>
+									<div class="input-group dates-wrap">
+										<input id="datepicker" class="dates form-control" id="exampleAmount" placeholder="Date & time" type="text">
+										<div class="input-group-prepend">
+											<span  class="input-group-text"><span class="lnr lnr-calendar-full"></span></span>
+										</div>
+									</div>
+									<textarea class="common-textarea form-control mt-10" name="message" placeholder="Messege" onfocus="this.placeholder = ''" onblur="this.placeholder = 'Messege'"></textarea>
+									<button  class="btn btn-default btn-lg btn-block text-center">Book Now!</button>
+								</form>
+						</div>
+					</div>
+				</div>
+			</section>
+			<!-- End booking Area -->
+
+			<!-- start footer Area -->
+			<?php include "footer.php" ?>
+			<!-- End footer Area -->
+
+
+			<script src="js/vendor/jquery-2.2.4.min.js"></script>
+			<!-- <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script> -->
+			<script src="js/vendor/bootstrap.min.js"></script>
+			<!-- <script type="text/javascript" src="https://maps.googleapis.com/maps/api/js?key=AIzaSyBhOdIF3Y9382fqJYt5I_sswSrEw5eihAA"></script> -->
+  			<script src="js/easing.min.js"></script>
+			<script src="js/hoverIntent.js"></script>
+			<script src="js/superfish.min.js"></script>
+			<script src="js/jquery.ajaxchimp.min.js"></script>
+			<script src="js/jquery.magnific-popup.min.js"></script>
+			<script src="js/owl.carousel.min.js"></script>
+			<script src="js/jquery.sticky.js"></script>
+			<!-- <script src="https://code.jquery.com/ui/1.12.1/jquery-ui.js"></script> -->
+			<script src="js/jquery.nice-select.min.js"></script>
+			<script src="js/parallax.min.js"></script>
+			<script src="js/waypoints.min.js"></script>
+			<script src="js/jquery.counterup.min.js"></script>
+			<script src="js/mail-script.js"></script>
+			<script src="js/main.js"></script>
+		</body>
+	</html>
---
+++ horseclub/header.php
@@ -0,0 +1,42 @@
+<header id="header" id="home">
+  <div class="container">
+    <div class="row header-top align-items-center">
+      <div class="col-lg-4 col-sm-4 menu-top-left">
+        <a href="mailto:krkunal29@gmail.com"><span class="lnr lnr-location"></span></a>
+        <a class="tel" href="mailto:krkunal29@gmail.com">krkunal29@gmail.com</a>
+      </div>
+      <div class="col-lg-4 menu-top-middle justify-content-center d-flex">
+      <a href="index.php">
+        <img class="img-fluid" src="img/logo.png" alt="">
+      </a>
+      </div>
+      <div class="col-lg-4 col-sm-4 menu-top-right">
+        <a class="tel" href="tel:+91-9766695099">+91-9766695099</a>
+        <a href="tel:+880 123 12 658 439"><span class="lnr lnr-phone-handset"></span></a>
+      </div>
+    </div>
+  </div>
+    <hr>
+  <div class="container">
+    <div class="row align-items-center justify-content-center d-flex">
+      <nav id="nav-menu-container">
+        <ul class="nav-menu">
+          <li class="menu-active"><a href="index.php">Home</a></li>
+          <li><a href="about.php">About</a></li>
+          <li><a href="service.php">Service</a></li>
+          <li><a href="training.php">training</a></li>
+          <li><a href="events.php">events</a></li>
+          <li><a href="pricing.php">Pricing</a></li>
+          <li class="menu-has-children"><a href="">Blog</a>
+            <ul>
+              <li><a href="blog-home.php">Blog Home</a></li>
+              <li><a href="blog-single.php">Blog Single</a></li>
+            </ul>
+          </li>
+          <li><a href="contact.php">Contact</a></li>
+          <li><a href="elements.php">Elements</a></li>
+        </ul>
+      </nav><!-- #nav-menu-container -->
+    </div>
+  </div>
+</header><!-- #header -->
